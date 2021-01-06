e90021192d883d831cd5a99cf56bf6c5d01703b1
Perrine Woringer <perrine.woringer@supelec.fr>
2021-01-06 10:09:24 +0100

    label nodes by indexes and convert to adjacency matrix

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from model import Model
 
 m = Model()
 
-print(m.data.nodes())
-print(m.data.edges())
+# print(m.graph.nodes())
+# print(m.graph.edges())
+#
+# print(nx.get_node_attributes(m.graph, "expression_level"))
+# print(nx.get_edge_attributes(m.graph, "alpha"))
+# print(nx.get_edge_attributes(m.graph, "beta"))
+#
+# print(m.graph.node["STE12"]["expression_level"])
+# for n in nx.all_neighbors(m.graph, "STE12"):
+#     print(n)
+# print(m.graph.adj["STE12"]["SRY1"]["alpha"])
 
-print(nx.get_node_attributes(m.data, "expression_level"))
-print(nx.get_edge_attributes(m.data, "alpha"))
-print(nx.get_edge_attributes(m.data, "beta"))
-
-print(m.data.node["STE12"]["expression_level"])
-for n in nx.all_neighbors(m.data, "STE12"):
-    print(n)
-print(m.data.adj["STE12"]["SRY1"]["alpha"])
+print(type(m.alpha))
+print(m.alpha)
--- model.py
+++ model.py
@@ -1,33 +1,44 @@
 import pandas as pd
 import networkx as nx
+import numpy as np
 
 class Model():
-    def __init__(self, path_edge_list="idea_ode_coefficients.tsv"):
-        self.full_graph = self.create_full_graph(path_edge_list)
-        self.data = self.create_subgraph()
-        self.initialize_expression_level()
+    def __init__(self, path_edge_list="idea_ode_coefficients.tsv", path_node_id="idea_wide_format_data.txt"):
+        full_graph = self.create_full_graph(path_edge_list, path_node_id)
+        self.graph = self.create_subgraph(full_graph)
+        self.alpha = nx.adjacency_matrix(self.graph, nodelist=None, weight="alpha")
+        self.beta = nx.adjacency_matrix(self.graph, nodelist=None, weight="beta")
+        self.y = self.initial_expression_level(self.graph.number_of_nodes())
 
-    def create_full_graph(self, path_edge_list):
+    def create_full_graph(self, path_edge_list, path_node_id):
         df = pd.read_csv(path_edge_list, sep="\t")
-        df.columns = ["cause","effect","alpha","beta"]
-        return nx.from_pandas_edgelist(df, source="cause", target="effect", edge_attr=["alpha", "beta"], create_using=nx.DiGraph())
+        df.columns = ["cause", "effect", "alpha", "beta"]
+        G = nx.from_pandas_edgelist(df, source="cause", target="effect", edge_attr=["alpha", "beta"], create_using=nx.DiGraph())
+        self.index_to_name = pd.read_csv(path_node_id, sep="\t", usecols=["GENE"])
+        # self.index_to_name = self.index_to_name[self.index_to_name["GENE"].isin(G.nodes())]
+        self.index_to_name.to_dict()["GENE"]
+        self.name_to_index = pd.read_csv(path_node_id, sep="\t", usecols=["GENE"]).reset_index().set_index("GENE").to_dict()["index"]
+        G = nx.relabel_nodes(G, self.name_to_index, copy=True)
+        nx.set_node_attributes(G, self.index_to_name, name="GENE")
+        return G
 
     lst = ['CLN3', 'CLN1', 'CLN2', 'CDH1', 'SWI5', 'CDC20', 'CLB5', 'CLB6', 'SIC1', 'CLB1', 'CLB2', 'MCM1']
 
-    def create_subgraph(self, list_of_start_genes=lst, method="include_neighbours_at_max_distance", max_distance=1):
+    def create_subgraph(self, full_graph, list_of_start_genes=lst, method="include_neighbours_at_max_distance", max_distance=1):
+        index_lst = [self.name_to_index[gene] for gene in list_of_start_genes]
         if method == "include_neighbours_at_max_distance":
             acc = set()
-            for gene in list_of_start_genes:
+            for gene in index_lst:
                 if max_distance == 1:
-                    acc = acc.union(set(nx.all_neighbors(self.full_graph, gene)))
+                    acc = acc.union(set(nx.all_neighbors(full_graph, gene)))
                 else:
-                    acc = acc.union(set(nx.ego_graph(self.full_graph, gene, radius=max_distance, center=True, undirected=True, distance=None).nodes()))
-            return self.full_graph.subgraph(list(acc))
+                    acc = acc.union(set(nx.ego_graph(full_graph, gene, radius=max_distance, center=True, undirected=True, distance=None).nodes()))
+            return full_graph.subgraph(list(acc))
         else:
             raise NotImplementedError
 
-    def initialize_expression_level(self):
-        nx.set_node_attributes(self.data, 0, name="expression_level") # TODO use a dict-like argument to specify different value for each node
+    def initial_expression_level(self, size):
+        return np.random.rand(size)
 
     def evolve(self):
         raise NotImplementedError
